51dabf4903b14438fecd4479e35064c4714b3123
sumyak jain <3456gaurav@gmail.com>
2019-04-17 01:12:34 +0530

    commit3

--- assesment.cpp
+++ assesment.cpp
@@ -399,6 +399,225 @@ void leap_year()
 
     return 0;
 }
+void sumof_naturalnum()
+    {
+    int n, sum = 0;
+
+    cout << "Enter a positive integer: ";
+    cin >> n;
+
+    for (int i = 1; i <= n; ++i) {
+        sum += i;
+    }
+
+    cout << "Sum = " << sum;
+    return 0;
+}
+void multiplication_table()
+{
+    int n;
+
+    cout << "Enter a positive integer: ";
+    cin >> n;
+
+    for (int i = 1; i <= 10; ++i) {
+        cout << n << " * " << i << " = " << n * i << endl;
+    }
+
+    return 0;
+}
+void Lcm()
+{
+    int n1, n2, max;
+
+    cout << "Enter two numbers: ";
+    cin >> n1 >> n2;
+
+    // maximum value between n1 and n2 is stored in max
+    max = (n1 > n2) ? n1 : n2;
+
+    do
+    {
+        if (max % n1 == 0 && max % n2 == 0)
+        {
+            cout << "LCM = " << max;
+            break;
+        }
+        else
+            ++max;
+    } while (true);
+
+    return 0;
+}
+void hcf()
+{
+    int n1, n2;
+
+    cout << "Enter two numbers: ";
+    cin >> n1 >> n2;
+
+    while(n1 != n2)
+    {
+        if(n1 > n2)
+            n1 -= n2;
+        else
+            n2 -= n1;
+    }
+
+    cout << "HCF = " << n1;
+    return 0;
+}
+void reverse_anumber()
+{
+    int n, reversedNumber = 0, remainder;
+
+    cout << "Enter an integer: ";
+    cin >> n;
+
+    while(n != 0)
+    {
+        remainder = n%10;
+        reversedNumber = reversedNumber*10 + remainder;
+        n /= 10;
+    }
+
+    cout << "Reversed Number = " << reversedNumber;
+
+    return 0;
+}
+  void powerofnum()
+  {
+    int exponent;
+    float base, result = 1;
+
+    cout << "Enter base and exponent respectively:  ";
+    cin >> base >> exponent;
+
+    cout << base << "^" << exponent << " = ";
+
+    while (exponent != 0) {
+        result *= base;
+        --exponent;
+    }
+
+    cout << result;
+
+    return 0;
+}
+void ASCIIvalue()
+{
+ char c;
+ cout << "Enter a character: ";
+ cin >> c;
+ cout << "ASCII Value of " << c << " is " << int(c);
+ return 0;
+}
+void product()
+{
+    double firstNumber, secondNumber, productOfTwoNumbers;
+    cout << "Enter two numbers: ";
+
+    // Stores two floating point numbers in variable firstNumber and secondNumber respectively
+    cin >> firstNumber >> secondNumber;
+
+    // Performs multiplication and stores the result in variable productOfTwoNumbers
+    productOfTwoNumbers = firstNumber * secondNumber;
+
+    cout << "Product = " << productOfTwoNumbers;
+
+    return 0;
+}
+void pyramid_usingstar()
+{
+    int rows;
+
+    cout << "Enter number of rows: ";
+    cin >> rows;
+
+    for(int i = rows; i >= 1; --i)
+    {
+        for(int j = 1; j <= i; ++j)
+        {
+            cout << "* ";
+        }
+        cout << endl;
+    }
+
+    return 0;
+}
+void largest_element()
+{
+    int i, n;
+    float arr[100];
+
+    cout << "Enter total number of elements(1 to 100): ";
+    cin >> n;
+    cout << endl;
+
+    // Store number entered by the user
+    for(i = 0; i < n; ++i)
+    {
+       cout << "Enter Number " << i + 1 << " : ";
+       cin >> arr[i];
+    }
+
+    // Loop to store largest number to arr[0]
+    for(i = 1;i < n; ++i)
+    {
+       // Change < to > if you want to find the smallest element
+       if(arr[0] < arr[i])
+           arr[0] = arr[i];
+    }
+    cout << "Largest element = " << arr[0];
+
+    return 0;
+}
+void transposeofmatrix()
+{
+    int a[10][10], trans[10][10], r, c, i, j;
+
+    cout << "Enter rows and columns of matrix: ";
+    cin >> r >> c;
+
+    // Storing element of matrix entered by user in array a[][].
+    cout << endl << "Enter elements of matrix: " << endl;
+    for(i = 0; i < r; ++i)
+    for(j = 0; j < c; ++j)
+    {
+        cout << "Enter elements a" << i + 1 << j + 1 << ": ";
+        cin >> a[i][j];
+    }
+
+    // Displaying the matrix a[][]
+    cout << endl << "Entered Matrix: " << endl;
+    for(i = 0; i < r; ++i)
+        for(j = 0; j < c; ++j)
+        {
+            cout << " " << a[i][j];
+            if(j == c - 1)
+                cout << endl << endl;
+        }
+
+    // Finding transpose of matrix a[][] and storing it in array trans[][].
+    for(i = 0; i < r; ++i)
+        for(j = 0; j < c; ++j)
+        {
+            trans[j][i]=a[i][j];
+        }
+
+    // Displaying the transpose,i.e, Displaying array trans[][].
+    cout << endl << "Transpose of Matrix: " << endl;
+    for(i = 0; i < c; ++i)
+        for(j = 0; j < r; ++j)
+        {
+            cout << " " << trans[i][j];
+            if(j == r - 1)
+                cout << endl << endl;
+        }
+
+    return 0;
+}
+
 };
 
 
