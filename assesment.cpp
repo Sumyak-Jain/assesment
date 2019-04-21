125d92ea19cdb0989563174b0435d052a645c2e1
ishitabansal <46719880+ishitabansal@users.noreply.github.com>
2019-04-21 04:29:21 -0500

    Update assesment.cpp

--- assesment.cpp
+++ assesment.cpp
@@ -832,6 +832,25 @@ void access_array_elements()
 
    return 0;
 }
+void rvereseArray(int arr[], int start, int end) 
+{ 
+    while (start < end) 
+    { 
+        int temp = arr[start];  
+        arr[start] = arr[end]; 
+        arr[end] = temp; 
+        start++; 
+        end--; 
+    }  
+}      
+  
+void printArray(int arr[], int size) 
+{ 
+   for (int i = 0; i < size; i++) 
+   cout << arr[i] << " "; 
+  
+   cout << endl; 
+}  
 
 
 };
